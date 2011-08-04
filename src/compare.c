c7548a035efc5c8610b9aee60334636464dd943d
Patrick Chkoreff <pc@loom.cc>
2011-08-04 10:24:07 -0400

    Now uses a completely generic self-configuring build script

--- src/compare.c
+++ src/compare.c
@@ -5,8 +5,12 @@
 #include "I.h"
 #include "R.h"
 
-/* I've found that it's faster *not* to cache the values (R C C), (C C), and
-(C (C I)) here.
+/*
+I tried making built-in combinators for the forms (R C C), (C C), and (C (C I))
+used here, but it had a negligible effect on performance, and in some cases
+even seemed slightly slower.  Maybe the built-in combinators had poorer
+locality of reference, which wiped out any gains we got from fewer calls to
+apply and fewer reduction steps.
 */
 
 void replace_compare(struct value *value, int cmp)
