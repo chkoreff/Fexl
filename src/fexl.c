91097a7b45c4212aae25b5e506c991b47b865083
Patrick Chkoreff <pc@loom.cc>
2012-03-30 08:08:08 -0400

    No need to apply the main function to a dummy value (I).

--- src/fexl.c
+++ src/fexl.c
@@ -1,5 +1,4 @@
 #include "value.h"
-#include "basic.h"
 #include "eval.h"
 #include "parse_file.h"
 #include "run.h"
@@ -8,14 +7,13 @@
 This is the main routine of the Fexl interpreter.  The exit status is 0 for
 success, or 1 for failure.
 
-It parses the file named on the command line, or stdin if no name is given.
-That yields a Fexl function which is applied to the "world" and then evaluated.
-In this case, the "world" is simply the identity function (I).
+It parses the file named on the command line, or stdin if no name is given, and
+evaluates that function.
 */
 int main(int argc, char *argv[], char *envp[])
 	{
 	beg_run(argc, argv, envp);
-	eval(A(parse_file(argc > 1 ? argv[1] : 0), Q(fexl_I)));
+	eval(parse_file(argc > 1 ? argv[1] : 0));
 	end_value();
 	return 0;
 	}
