580a0a8334af10e4c9e84625c76c5dd5801558ea
Patrick Chkoreff <pc@loom.cc>
2013-10-11 15:19:46 -0400

    Merge the libraries with suffix "2" up into the corresponding main library.  No need for that kind of hair-splitting.

--- src/qfile.c
+++ src/qfile.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
+#include "file.h"
 #include "str.h"
 
 #include "value.h"
 #include "basic.h"
+#include "long.h"
 #include "qfile.h"
+#include "qstr.h"
 
 value type_file(value f)
 	{
@@ -27,3 +30,128 @@ value Qfile(FILE *fh)
 	{
 	return V(type_file,0,(value)fh);
 	}
+
+/* (putchar ch) Write character to stdout. */
+value type_putchar(value f)
+	{
+	if (!f->L) return f;
+	value x = eval(f->R);
+	(void)putchar(get_long(x));
+	drop(x);
+	return I;
+	}
+
+/* getchar : Return next char from stdin. */
+value type_getchar(value f)
+	{
+	return Qlong(getchar());
+	}
+
+value type_stdin(value f)  { return Qfile(stdin); }
+value type_stdout(value f) { return Qfile(stdout); }
+value type_stderr(value f) { return Qfile(stderr); }
+
+/* (fputc file ch) Write character to stdout. */
+value type_fputc(value f)
+	{
+	if (!f->L || !f->L->L) return f;
+	value x = eval(f->L->R);
+	value y = eval(f->R);
+
+	FILE *fh = get_file(x);
+	(void)fputc(get_long(y),fh);
+
+	drop(x);
+	drop(y);
+	return I;
+	}
+
+/* (fgetc fh) returns the next character from the file handle. */
+value type_fgetc(value f)
+	{
+	if (!f->L) return f;
+	value x = eval(f->R);
+
+	FILE *fh = get_file(x);
+	int ch = fgetc(fh);
+
+	drop(x);
+	return Qlong(ch);
+	}
+
+/* (fwrite fh str) Write string to file handle. */
+value type_fwrite(value f)
+	{
+	if (!f->L || !f->L->L) return f;
+	value x = eval(f->L->R);
+	value y = eval(f->R);
+
+	FILE *fh = get_file(x);
+	struct str *str = get_str(y);
+	size_t count = fwrite(str->data, 1, str->len, fh);
+	(void)count;  /* Ignore the return count. */
+
+	drop(x);
+	drop(y);
+	return I;
+	}
+
+/* (fopen path mode) Open a file and return no or (yes fh). */
+value type_fopen(value f)
+	{
+	if (!f->L || !f->L->L) return f;
+	value x = eval(f->L->R);
+	value y = eval(f->R);
+
+	struct str *path = get_str(x);
+	struct str *mode = get_str(y);
+
+	FILE *fh = fopen(path->data, mode->data);
+	value g;
+	if (fh)
+		g = A(C,yield(I,Qfile(fh)));
+	else
+		g = C;
+
+	drop(x);
+	drop(y);
+	return g;
+	}
+
+value type_readlink(value f)
+	{
+	if (!f->L) return f;
+	value x = eval(f->R);
+	value y = Qstr(safe_readlink(get_str(x)->data));
+	drop(x);
+	return y;
+	}
+
+/*LATER more functions */
+
+/*
+LATER fclose : (not necessary due to auto-close, but gives you some control.
+Set the right side FILE * to 0 afterward.
+*/
+
+/*LATER fdopen */
+/*LATER fflush */
+/*LATER file_string */
+/*LATER fmemopen */
+
+#if 0
+
+/* sample code here */
+{
+char *s = "abcdefg";
+FILE *fp = fmemopen(s, strlen(s), "r");
+int ch;
+while (1)
+	{
+	ch = fgetc(fp);
+	if (ch == -1) break;
+	printf("ch='%c'\n", ch);
+	}
+fclose(fp);
+}
+#endif
--- src/qstr.c
+++ src/qstr.c
@@ -3,6 +3,7 @@
 
 #include "value.h"
 #include "basic.h"
+#include "long.h"
 #include "qstr.h"
 
 /*LATER wrap a combinator around the buf.c module, which will allow very fast
@@ -41,3 +42,57 @@ value type_string_append(value f)
 	drop(y);
 	return z;
 	}
+
+/* (string_at str pos) is the character at pos, or 0 if out of bounds. */
+value type_string_at(value f)
+	{
+	if (!f->L || !f->L->L) return f;
+	value x = eval(f->L->R);
+	value y = eval(f->R);
+	value z = Qlong(str_at(get_str(x),get_long(y)));
+	drop(x);
+	drop(y);
+	return z;
+	}
+
+/* (string_cmp x y) compares x and y and returns <0, 0, or >0. */
+value type_string_cmp(value f)
+	{
+	if (!f->L || !f->L->L) return f;
+	value x = eval(f->L->R);
+	value y = eval(f->R);
+	value z = Qlong(str_cmp(get_str(x),get_str(y)));
+	drop(x);
+	drop(y);
+	return z;
+	}
+
+/* (string_len x) is the length of string x */
+value type_string_len(value f)
+	{
+	if (!f->L) return f;
+	value x = eval(f->R);
+	value z = Qlong(get_str(x)->len);
+	drop(x);
+	return z;
+	}
+
+/* Compute the length of the longest common prefix of two strings. */
+value type_string_common(value f)
+	{
+	if (!f->L || !f->L->L) return f;
+	value x = eval(f->L->R);
+	value y = eval(f->R);
+	value z = Qlong(str_common(get_str(x),get_str(y)));
+	drop(x);
+	drop(y);
+	return z;
+	}
+
+#if 0
+LATER more functions
+string_slice str pos len
+string_index
+string_long
+string_double
+#endif
