163f6abe98facead9efe66b6d027db048912e62f
Patrick Chkoreff <pc@fexl.com>
2015-05-31 11:59:26 -0400

    Now using long-reach substitution patterns.
    The efficiency gains in this release are fairly small: test/fib runs about 2%
    faster, and test/checkt runs about 0.5% faster.
    
    However, I think this is just the beginning.  Now that we're using C function
    pointers in the patterns, we should be able to optimize common substitution
    patterns by implementing new builder functions and using them in fuse.
    
    Also, for what it's worth, the new executable is 780 bytes smaller, which
    probably reflects the simplification of the subst routine, which now simply
    calls a function pointer instead of doing pattern matching.

--- src/pattern.c
+++ src/pattern.c
@@ -1,33 +1,83 @@
 #include <value.h>
-#include <basic.h>
 #include <pattern.h>
 
+/* A builder is a function that uses pattern p to build a copy of form f with
+argument x substituted in various places. */
+typedef value (*builder)(value p, value f, value x);
+
+/* Here are some builders that perform some common substitutions. */
+
+static value subst_0(value p, value f, value x)
+	{
+	(void)p;
+	(void)x;
+	return hold(f);
+	}
+
+static value subst_1(value p, value f, value x)
+	{
+	(void)p;
+	(void)f;
+	return hold(x);
+	}
+
+static value subst_110(value p, value f, value x)
+	{
+	return A(subst(p->L,f->L,x),hold(f->R));
+	}
+
+static value subst_101(value p, value f, value x)
+	{
+	return A(hold(f->L),subst(p->R,f->R,x));
+	}
+
+static value subst_111(value p, value f, value x)
+	{
+	return A(subst(p->L,f->L,x),subst(p->R,f->R,x));
+	}
+
 /* Make a pattern that ignores the form and returns the argument. */
-value none(void) { return Q(type_C); }
+value none(void) { return Q((type)subst_0); }
 
 /* Make a pattern that returns the form and ignores the argument. */
-value here(void) { return Q(type_I); }
+value here(void) { return Q((type)subst_1); }
+
+/* Make a pattern with the given builder and left and right sides. */
+static value P(builder b, value p, value q)
+	{
+	value r = A(p,q);
+	r->T = (type)b;
+	return r;
+	}
+
+/* Return true if the pattern is none. */
+static int is_none(value p)
+	{
+	return (builder)p->T == subst_0;
+	}
 
 /* Make a pattern that sends the argument to the left and right as needed. */
 value fuse(value p, value q)
 	{
-	if (p->T == type_C && q->T == type_C)
+	if (is_none(p))
 		{
-		drop(p);
-		drop(q);
-		return none();
+		if (is_none(q))
+			{
+			drop(p);
+			drop(q);
+			return none();
+			}
+		else
+			return P(subst_101,p,q);
 		}
+	else if (is_none(q))
+		return P(subst_110,p,q);
 	else
-		return A(p,q);
+		return P(subst_111,p,q);
 	}
 
-/* Make a copy of f, but substitute x wherever I appears in pattern p. */
+/* Use pattern p to make a copy of f with x substituted in various places. */
 value subst(value p, value f, value x)
 	{
-	if (p->T == type_C)
-		return hold(f);
-	else if (p->T == type_I)
-		return hold(x);
-	else
-		return A(subst(p->L,f->L,x),subst(p->R,f->R,x));
+	return ((builder)p->T)(p,f,x);
 	}
