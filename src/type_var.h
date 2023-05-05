2a42cf9935d21c56fe389951251c43c037f181f7
Patrick Chkoreff <pc@fexl.com>
2023-05-05 15:48:43 -0400

    /home/patrick/msg

--- src/type_var.h
+++ src/type_var.h
@@ -1,6 +1,7 @@
 extern value type_var(value f);
 extern value type_var_new(value f);
 extern value type_var_get(value f);
+extern value type_var_getf(value f);
 extern value type_var_put(value f);
 extern value type_var_putf(value f);
 extern value type_is_var(value f);
